4cc319f364df2b4d407397865d41278758bcd442
gabriel <gabrieloliveiravicente2015@gmail.com>
2025-03-06 11:37:17 -0300

    docs: reimplementing swagger docs, removing autogen

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,25 @@
 import express, { Application, Request, Response } from 'express';
 import swaggerUi from 'swagger-ui-express';
+import swaggerOptions from './swagger';
+import swaggerJSDoc from 'swagger-jsdoc';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
-// Load environment variables
 dotenv.config();
 
 const app: Application = express();
 
-// Middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Basic route
 app.get('/', (req: Request, res: Response) => {
     res.json({ message: 'Welcome to TimeBank API' });
 });
 
-// Swagger Documentation setup
-const swaggerFile = require('./swagger_output.json');
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 const PORT = process.env.PORT || 3000;
 
