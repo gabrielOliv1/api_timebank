50c7b22458044b8f71e47edc1f532613a4e04d15
gabriel <gabrieloliveiravicente2015@gmail.com>
2025-03-18 15:35:54 -0300

    fix: changing the frontend localhost port

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,7 @@ dotenv.config();
 const app: Application = express();
 
 app.use(cors({
-    origin: ['http://localhost:3001'],
+    origin: ['http://localhost:5173'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
