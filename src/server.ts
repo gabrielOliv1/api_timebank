b548b646017fedfa35888b9e026a469308499cf0
gabriel <gabrieloliveiravicente2015@gmail.com>
2025-03-24 17:58:46 -0300

    fix: enabling cors for frontend url

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,10 @@ dotenv.config();
 const app: Application = express();
 
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: [
+        'http://localhost:5173',
+        'https://timebank.vercel.app'
+    ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
